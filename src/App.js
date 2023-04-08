223f263406e8d9fec4031dfd2eb487e06be3214d
mohanrajkpm <infomohanrajsoft@gmail.com>
2023-04-08 10:41:13 +0530

    React client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import socketIOClient from 'socket.io-client'
+import Chart from 'chart.js/auto'
+import { CategoryScale } from 'chart.js';
+import BarChart from './components/bar_chart';
+import LineChart from './components/line_chart';
+
+Chart.register(CategoryScale);
 
 function App() {
+  
+  const [data, setData] = useState({
+    labels: [0],
+    datasets: [
+      {
+        label: 'Users Profit',
+        data: [0],
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "&quot;#ecfof1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71do"
+        ],
+        boarderColor: 'black',
+        borderwidth: 2
+      }
+    ]
+  });
+
+  useEffect(() => {
+    const socket = socketIOClient('http://127.0.0.1:4001/')
+    socket.on('message', (data) =>{
+    const chartData = {   labels: data.map((item) => item.year),
+      datasets: [
+        {
+          label: 'Users Profit',
+          data: data.map((item) => item.userGain),
+          backgroundColor: [
+            "rgba(75,192,192,1)",
+            "&quot;#ecfof1",
+            "#50AF95",
+            "#f3ba2f",
+            "#2a71do"
+          ],
+          boarderColor: 'black',
+          borderwidth: 2
+        }
+      ]
+    }
+    setData(chartData)
+    });
+  },[])
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BarChart chartData={data} />
+      <LineChart chartData={data} />
     </div>
   );
 }
